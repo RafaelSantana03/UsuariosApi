65e52a731743f0a1799d2ea23513394039f82d0a
Rafael Santana <rafa03santana@gmail.com>
2026-03-28 00:21:21 -0300

    feat: adicionando repository

---
+++ UsuariosApi/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using UsuariosApi.Models;
+
+namespace UsuariosApi.Repositories;
+
+public interface IUsuarioRepository
+{
+    Usuario? ObterPorEmail(string email);
+    void Criar (Usuario usuario);
+    Usuario? ObterPorId(int id);
+}
---
+++ UsuariosApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,28 @@
+﻿using UsuariosApi.Data;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Repositories;
+
+public class UsuarioRepository : IUsuarioRepository
+{
+    private readonly AppDbContext _context;
+    public UsuarioRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Usuario? ObterPorEmail(string email)
+    {
+        return _context.Usuarios.FirstOrDefault(u => u.Email == email);
+    }
+    public void Criar(Usuario usuario)
+    {
+        _context.Usuarios.Add(usuario);
+        _context.SaveChanges();
+    }
+
+    public Usuario? ObterPorId(int id)
+    {
+        return _context.Usuarios.Find(id);
+    }
+}
