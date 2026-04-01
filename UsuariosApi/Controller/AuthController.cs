e267ae50984b0339256916f498c96ce08a253cc9
Rafael Santana <rafa03santana@gmail.com>
2026-04-01 08:08:21 -0300

    feat:Adiciona os controllers

---
+++ UsuariosApi/Controller/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.DTOs;
+using UsuariosApi.Service;
+
+namespace UsuariosApi.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Register")]
+        public ActionResult<UsuarioResponseDto> Register(RegisterRequestDto dto)
+        {
+            try
+            {
+                var resultado = _authService.Register(dto);
+                return CreatedAtAction(nameof(Register), resultado);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("Login")]
+        public ActionResult<string> Login(LoginRequestDto dto)
+        {
+            try
+            {
+                var token = _authService.Login(dto);
+                return Ok(new { token });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpGet("Perfil")]
+        public ActionResult Perfil()
+        {
+            var id = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            var nome = User.FindFirst(System.Security.Claims.ClaimTypes.Name)?.Value;
+            var email = User.FindFirst(System.Security.Claims.ClaimTypes.Email)?.Value;
+
+            return Ok(new { id, nome, email });
+        }
+    }
+}
